f39dc7efdd166f92f84455d8f8a51785d46612be
marcus <marcus@banktrustdeals.com>
2026-01-15 03:05:48 +0600

    zoommeeting

--- src/components/ZoomMeetingEmbed.tsx
+++ src/components/ZoomMeetingEmbed.tsx
@@ -36,14 +36,11 @@ const MeetingComponent = ({ meetingConfig, signature }) => {
         console.log(error);
       }
     }); */
-  }, [meetingConfig, signature]);
+  //}, [meetingConfig, signature]);
+  });
 
   return (
-    <div>
-      <noscript>You need to enable JavaScript to run this app.</noscript>
-	  <div id="zmmtg-root"></div>
-	  <div id="root"></div>
-    </div>
+   
   );
 };
 
