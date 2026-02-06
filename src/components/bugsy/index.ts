c3b635ab68cb209b32cd28f12f36b0fcf5d919aa
hawk7227 <marcus@banktrustdeals.com>
2026-02-06 13:35:17 -0700

    Update index.ts

--- src/components/bugsy/index.ts
+++ src/components/bugsy/index.ts
@@ -1,1 +1,24 @@
+// ============================================================================
+// BUGSY COMPONENTS - Export Index
+// Version: 2.0.0
+// ============================================================================
+
+// Main Components
+export { default as BugsyWidget } from './BugsyWidget';
+export { default as BugsyModal } from './BugsyModal';
+
+// Interview Experience
+export { default as BugsyInterviewOverlay } from './BugsyInterviewOverlay';
+export { default as BugsyCharacter } from './BugsyCharacter';
+export { default as ChatBubble } from './ChatBubble';
+
+// Screen Components
+export { default as BugsyIdleScreen } from './screens/BugsyIdleScreen';
+export { default as BugsyRecordingScreen } from './screens/BugsyRecordingScreen';
+export { default as BugsyProcessingScreen } from './screens/BugsyProcessingScreen';
+export { default as BugsyReflectionScreen } from './screens/BugsyReflectionScreen';
+export { default as BugsyClarifyScreen } from './screens/BugsyClarifyScreen';
+export { default as BugsyVerificationScreen } from './screens/BugsyVerificationScreen';
+export { default as BugsySuccessScreen } from './screens/BugsySuccessScreen';
+export { default as BugsyErrorScreen } from './screens/BugsyErrorScreen';
 
